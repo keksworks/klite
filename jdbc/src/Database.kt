2f11a902bd43b1400dfa52bb29bd88d2bfd382ea
Anton Keks <anton@azib.net>
2025-12-28 17:27:41 +0200

    implement update() via Dml

--- jdbc/src/Database.kt
+++ jdbc/src/Database.kt
@@ -10,18 +10,16 @@ class Database private constructor(val db: DataSource) {
 
   fun query(@Language("SQL") select: String) = Query<Any>(StringBuilder(select))
   fun select(@Language("SQL", prefix = selectFrom) table: String) = Query<Any>(StringBuilder(selectFrom).append(q(table)))
+  
+  fun update(@Language("SQL", prefix = selectFrom) table: String) = Dml(StringBuilder("update " + q(table)))
 
   inner class Query<R> internal constructor(
     @Language("SQL") select: StringBuilder,
     mapper: Mapper<R> = { create() }
-  ): QueryExecutor<R>(select, mapper) {
+  ): QueryExecutor<R>(select, mapper), WhereHandler<Query<R>> {
     fun join(@Language("SQL", prefix = selectFrom) table: String, on: String) = this.also {
       select.append(" join ").append(q(table)).append(" on ").append(on)
     }
-
-    fun where(where: Where) = this.also { this.where += whereConvert(where) }
-    fun where(vararg where: ColValue?) = where(where.filterNotNull())
-    fun where(where: ColValue?) = this.also { where?.let { this.where += it } }
 
     fun suffix(@Language("SQL", prefix = selectFromTable) suffix: String) = this.also { this.suffix.append(' ').append(suffix) }
     fun order(by: ColName, asc: Boolean = true) = suffix("order by " + q(name(by)) + (if (asc) "" else " desc" ))
@@ -35,7 +33,7 @@ class Database private constructor(val db: DataSource) {
     @Language("SQL") val select: StringBuilder,
     protected var mapper: Mapper<R>
   ) {
-    protected val where = mutableListOf<ColValue>()
+    val where = mutableListOf<ColValue>() // TODO: not public
     protected var suffix = StringBuilder()
 
     fun run(): Sequence<R> = sequence {
@@ -51,6 +49,24 @@ class Database private constructor(val db: DataSource) {
     fun list() = run().toList()
 
     fun one() = run().firstOrNull() ?: throw NoSuchElementException("Not found")
+  }
+
+  interface WhereHandler<P: WhereHandler<P>> {
+    val where: MutableList<ColValue>
+
+    fun where(where: Where): P = (this as P).also { this.where += whereConvert(where) }
+    fun where(vararg where: ColValue?): P = where(where.filterNotNull())
+    fun where(where: ColValue?): P = (this as P).also { where?.let { this.where += it } }
+  }
+
+  inner class Dml internal constructor(@Language("SQL") val expr: StringBuilder): WhereHandler<Dml> {
+    override val where = mutableListOf<ColValue>() // TODO: not public
+    protected val values = mutableMapOf<ColName, Any?>()
+
+    fun set(value: ColValue) = this.also { values += value }
+    fun set(vararg values: ColValue) = this.also { values.forEach { v -> this.values += v } }
+
+    fun run() = db.exec(expr.toString() + setExpr(values) + whereExpr(where), setValues(values), whereValues(where))
   }
 }
 
