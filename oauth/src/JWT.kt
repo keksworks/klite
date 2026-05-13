e3866c0a7f2a0e9f30c46e184e65b74517e9a45b
Anton Keks <anton@azib.net>
2026-05-13 18:48:46 +0300

    use get() for issuer for consistency

--- oauth/src/JWT.kt
+++ oauth/src/JWT.kt
@@ -40,7 +40,7 @@ class JWT(private val token: String) {
     val subject get() = getString("sub")
     val audience get() = getString("aud")
     val issuedAt get() = getOrNull<Number>("iat")?.let { Instant.ofEpochSecond(it.toLong()) }
-    val issuer = getOrNull<String>("iss")
+    val issuer get() = getOrNull<String>("iss")
     val expiresAt get() = getOrNull<Number>("exp")?.let { Instant.ofEpochSecond(it.toLong()) }
     val name get() = getOrNull<String>("name")
     val email get() = getOrNull<String>("email")?.let { Email(it) }
