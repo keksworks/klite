fb6a81e19949b6205acdf860b3c66818092a10b5
Anton Keks <anton@azib.net>
2026-06-22 16:14:22 +0300

    add logging to OpenID discovery

--- oauth/src/OpenID.kt
+++ oauth/src/OpenID.kt
@@ -1,6 +1,8 @@
 package klite.oauth
 
+import klite.info
 import klite.json.parse
+import klite.logger
 import klite.oauth.JWT.Companion.jsonMapper
 import java.net.URI
 
@@ -22,16 +24,19 @@ class OpenIDConfig(
 )
 
 class OpenID(private val configUrl: URI) {
+  private val log = logger()
   val config: OpenIDConfig = readConfig()
   val keys: List<JwkKey> = readKeys()
 
   fun key(kid: String) = keys.find { it.kid == kid }
 
   private fun readConfig() = configUrl.toURL().openStream().use { stream ->
+    log.info("Fetching config from $configUrl")
     jsonMapper.parse<OpenIDConfig>(stream)
   }
 
   private fun readKeys() = config.jwksUri.toURL().openStream().use { stream ->
+    log.info("Fetching keys ${config.jwksUri}")
     jsonMapper.parse<JwksKeysResponse>(stream).keys
   }
 }
--- oauth/test/OpenIDTest.kt
+++ oauth/test/OpenIDTest.kt
@@ -1,4 +1,5 @@
 import ch.tutteli.atrium.api.fluent.en_GB.toEqual
+import ch.tutteli.atrium.api.fluent.en_GB.toHaveSize
 import ch.tutteli.atrium.api.verbs.expect
 import klite.oauth.OpenID
 import org.junit.jupiter.api.Test
@@ -10,5 +11,6 @@ class OpenIDTest {
     expect(openId.config.authorizationEndpoint).toEqual(URI("https://tara.ria.ee/oidc/authorize"))
     expect(openId.config.tokenEndpoint).toEqual(URI("https://tara.ria.ee/oidc/token"))
     expect(openId.config.jwksUri).toEqual(URI("https://tara.ria.ee/oidc/jwks"))
+    expect(openId.keys.map { it.publicKey }).toHaveSize(2)
   }
 }
