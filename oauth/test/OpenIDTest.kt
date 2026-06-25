5147f71111e915861690d0e4eaef0df7faa3a98e
Anton Keks <anton@azib.net>
2026-06-25 10:18:29 +0300

    add testfor Google OIDC endpoint

--- oauth/test/OpenIDTest.kt
+++ oauth/test/OpenIDTest.kt
@@ -6,6 +6,15 @@ import org.junit.jupiter.api.Test
 import java.net.URI
 
 class OpenIDTest {
+  @Test fun `read Google config`() {
+    val openId = OpenID(URI("https://accounts.google.com"))
+    expect(openId.config.issuer).toEqual(URI("https://accounts.google.com"))
+    expect(openId.config.authorizationEndpoint).toEqual(URI("https://accounts.google.com/o/oauth2/v2/auth"))
+    expect(openId.config.tokenEndpoint).toEqual(URI("https://oauth2.googleapis.com/token"))
+    expect(openId.config.jwksUri).toEqual(URI("https://www.googleapis.com/oauth2/v3/certs"))
+    expect(openId.keys.map { it.publicKey }).toHaveSize(4)
+  }
+
   @Test fun `read TARA config`() {
     val openId = OpenID(URI("https://tara.ria.ee/oidc"))
     expect(openId.config.issuer).toEqual(URI("https://tara.ria.ee"))
