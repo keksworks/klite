7d5012f3305a104fdf770e5cd9af096ecfc5b05d
Anton Keks <anton@azib.net>
2026-07-26 16:59:17 +0300

    skip meta where everything is false; and optimize toDBValues() if no special annotations are present on the class

--- jdbc/src/Values.kt
+++ jdbc/src/Values.kt
@@ -7,12 +7,14 @@ import kotlin.reflect.KClass
 import kotlin.reflect.KProperty1
 import kotlin.reflect.full.findAnnotation
 
-private data class AnnotationMeta(val isJson: Boolean, val isFlatten: Boolean)
+private data class AnnotationMeta(val isJson: Boolean, val isFlatten: Boolean) {
+  val some get() = isJson || isFlatten
+}
 private val annotationMetaCache = ConcurrentHashMap<KClass<*>, Map<String, AnnotationMeta>>()
 
 private fun KClass<*>.annotationMeta() = annotationMetaCache[this] ?: publicProperties.mapValues { (_, p) ->
   AnnotationMeta(p.findAnnotation<JsonColumn>() != null, p.findAnnotation<FlattenColumns>() != null)
-}.also { annotationMetaCache[this] = it }
+}.filter { it.value.some }.also { annotationMetaCache[this] = it }
 
 inline fun <reified T: Any> ResultSet.create(vararg provided: PropValue<T, *>) = create(T::class, *provided)
 
@@ -26,8 +28,8 @@ fun <T: Any> ResultSet.create(type: KClass<T>, vararg provided: PropValue<T, *>,
     val prop = type.publicProperties[it.name]
     val column = columnPrefix + (prop?.colName ?: it.name)
     if (extraArgs.containsKey(it.name)) extraArgs[it.name!!]
-    else if (prop != null && meta[it.name]!!.isJson) getJsonOrNull(column, it.type)
-    else if (prop != null && meta[it.name]!!.isFlatten) create(it.type.classifier as KClass<T>, *provided, columnPrefix = columnPrefix)
+    else if (prop != null && meta[it.name]?.isJson == true) getJsonOrNull(column, it.type)
+    else if (prop != null && meta[it.name]?.isFlatten == true) create(it.type.classifier as KClass<T>, *provided, columnPrefix = columnPrefix)
     else if (it.isOptional) getOptional<T>(column, it.type).getOrDefault(AbsentValue)
     else get(column, it.type)
   }
@@ -36,9 +38,10 @@ fun <T: Any> ResultSet.create(type: KClass<T>, vararg provided: PropValue<T, *>,
 fun <T: Any> T.toDBValues(vararg provided: PropValue<T, *>, skip: Collection<KProperty1<T, *>> = emptyList()): Map<KProperty1<T, *>, Any?> {
   val values = toValues(*provided, skip = skip) as MutableMap
   val meta = this::class.annotationMeta()
-  values.entries.forEach { (prop, v) ->
-    if (meta[prop.name]!!.isJson) values[prop] = jsonb(v)
-    else if (v != null && meta[prop.name]!!.isFlatten) {
+  if (meta.isEmpty()) return values
+  values.entries.toList().forEach { (prop, v) ->
+    if (meta[prop.name]?.isJson == true) values[prop] = jsonb(v)
+    else if (v != null && meta[prop.name]?.isFlatten == true) {
       values.remove(prop)
       values += v.toDBValues() as Map<KProperty1<T, *>, Any?>
     }
