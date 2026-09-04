10057e61c0dd10cba046268951fd7bb0eb451459
Anton Keks <anton@azib.net>
2026-09-04 10:07:19 +0300

    leave only one version of each lock functions

--- jdbc/src/PostgresExtensions.kt
+++ jdbc/src/PostgresExtensions.kt
@@ -3,7 +3,6 @@ package klite.jdbc
 import klite.logger
 import klite.trimToNull
 import klite.warn
-import java.sql.Connection
 import java.sql.ResultSet
 import java.sql.Types
 import java.util.concurrent.ConcurrentHashMap
@@ -18,16 +17,11 @@ val DataSource.isPostgres get() = dbPostgresIndicators.getOrPut(this) {
   (url ?: withConnection { metaData.url }).contains("postgresql")
 }
 
-fun Connection.lock(on: String) { call("pg_advisory_lock", lockKey(on)) }
-fun Connection.tryLock(on: String): Boolean = call("pg_try_advisory_lock", lockKey(on), returnSqlType = Types.BOOLEAN) == true
-fun Connection.unlock(on: String): Boolean = (call("pg_advisory_unlock", lockKey(on), returnSqlType = Types.BOOLEAN) == true).also {
+fun DB.lock(on: String) { call("pg_advisory_lock", lockKey(on)) }
+fun DB.tryLock(on: String): Boolean = call("pg_try_advisory_lock", lockKey(on), returnSqlType = Types.BOOLEAN) == true
+fun DB.unlock(on: String): Boolean = (call("pg_advisory_unlock", lockKey(on), returnSqlType = Types.BOOLEAN) == true).also {
   if (!it) logger().warn("Unlocking of $on failed in $this")
 }
-
-// TODO: should these be deprecated? unlock() must be run always on the same connection
-fun DataSource.lock(on: String) = withConnection { lock(on) }
-fun DataSource.tryLock(on: String): Boolean = withConnection { tryLock(on) }
-fun DataSource.unlock(on: String): Boolean = withConnection { unlock(on) }
 
 private fun lockKey(on: String): Long = on.fold(0L) { acc, char -> 31L * acc + char.code }
 
