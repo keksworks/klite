291c85eaaf409efefa58103d418029290badf8a8
Anton Keks <anton@azib.net>
2026-09-07 15:10:38 +0300

    split pg and non-pg tests

--- jdbc/test/SqlExprTest.kt
+++ jdbc/test/SqlExprTest.kt
@@ -3,11 +3,10 @@ package klite.jdbc
 import ch.tutteli.atrium.api.fluent.en_GB.notToEqual
 import ch.tutteli.atrium.api.fluent.en_GB.toEqual
 import ch.tutteli.atrium.api.verbs.expect
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class SqlExprTest {
-  val pgDb = ConfigDataSource("jdbc:postgres")
-
   @Test fun equals() {
     expect(SqlExpr("expr", 1, 2, 3)).toEqual(SqlExpr("expr", 1, 2, 3))
     expect(SqlOp(">", 1)).toEqual(SqlOp(">", 1))
@@ -16,40 +15,58 @@ class SqlExprTest {
     expect(In(123, 234)).toEqual(In(123, 234))
     expect(NotIn(123, 234)).toEqual(NotIn(123, 234))
     expect(NotIn(123, 234) as SqlExpr).notToEqual(In(123, 234))
-
     expect(SqlExpr("expr", 1, 2, 3).hashCode()).toEqual(3158614)
   }
 
-  @Test fun orExpr() {
-    val or = orExpr("column" to null, "column" to listOf(1, 2, 3), null)
-    expect(or.expr(pgDb, "")).toEqual("(\"column\" is null or \"column\"" +
-      if (pgDb.isPostgres) " = any(?))" else " in (?, ?, ?))")
-    val expectedValues: List<Any?> = if (pgDb.isPostgres) listOf(listOf(1, 2, 3)) else listOf(1, 2, 3)
-    expect(or.values.toList()).toEqual(expectedValues)
+  @Nested inner class `postgres` {
+    val db = ConfigDataSource("jdbc:postgres")
+
+    @Test fun orExpr() {
+      val or = orExpr("column" to null, "column" to listOf(1, 2, 3), null)
+      expect(or.expr(db, "")).toEqual("(\"column\" is null or \"column\" = any(?))")
+      expect(or.values(db).toList()).toEqual(listOf<Any?>(listOf(1, 2, 3)))
+    }
+
+    @Test fun inOperators() {
+      expect(In(1, 2, 3).expr(db, "column")).toEqual("\"column\" = any(?)")
+      expect(NotIn(1, 2, 3).expr(db, "column")).toEqual("\"column\" <> all(?)")
+    }
+
+    @Test fun expressionValuesBindingStrategy() {
+      val and = andExpr("column" to Between(1, 2), "array" to listOf(3, 4))
+      expect(and.expr(db, "")).toEqual("(\"column\" between ? and ? and array = any(?))")
+      expect(and.values(db).toList()).toEqual(listOf<Any?>(1, 2, listOf(3, 4)))
+    }
+
+    @Test fun SqlComputed() {
+      expect(SqlComputed("current_date").expr(db, "date")).toEqual("date=current_date")
+    }
+
+    @Test fun SqlOp() {
+      expect(SqlOp("<", 1).expr(db, "n")).toEqual("n < ?")
+      expect(SqlOp("<=", SqlComputed("current_date")).expr(db, "date")).toEqual("date <= current_date")
+      expect(("date" lte SqlComputed("123")).second.expr(db, "x")).toEqual("x <= 123")
+    }
   }
 
-  @Test fun inOperators() {
-    expect(In(1, 2, 3).expr(pgDb, "column")).toEqual("\"column\"" +
-      if (pgDb.isPostgres) " = any(?)" else " in (?, ?, ?)")
-    expect(NotIn(1, 2, 3).expr(pgDb, "column")).toEqual("\"column\"" +
-      if (pgDb.isPostgres) " <> all(?)" else " not in (?, ?, ?)")
-  }
+  @Nested inner class `non-postgres` {
+    val db = ConfigDataSource("jdbc:h2:mem:test")
 
-  @Test fun expressionValuesBindingStrategy() {
-    val and = andExpr("column" to Between(1, 2), "array" to listOf(3, 4))
-    expect(and.expr(pgDb, "")).toEqual("(\"column\" between ? and ? and array" +
-      if (pgDb.isPostgres) " = any(?))" else " in (?, ?))")
-    val expectedValues: List<Any?> = if (pgDb.isPostgres) listOf(1, 2, listOf(3, 4)) else listOf(1, 2, 3, 4)
-    expect(and.values.toList()).toEqual(expectedValues)
-  }
+    @Test fun orExpr() {
+      val or = orExpr("column" to null, "column" to listOf(1, 2, 3), null)
+      expect(or.expr(db, "")).toEqual("(\"column\" is null or \"column\" in (?, ?, ?))")
+      expect(or.values(db).toList()).toEqual(listOf<Any?>(1, 2, 3))
+    }
 
-  @Test fun SqlComputed() {
-    expect(SqlComputed("current_date").expr(pgDb, "date")).toEqual("date=current_date")
-  }
+    @Test fun inOperators() {
+      expect(In(1, 2, 3).expr(db, "column")).toEqual("\"column\" in (?, ?, ?)")
+      expect(NotIn(1, 2, 3).expr(db, "column")).toEqual("\"column\" not in (?, ?, ?)")
+    }
 
-  @Test fun SqlOp() {
-    expect(SqlOp("<", 1).expr(pgDb, "n")).toEqual("n < ?")
-    expect(SqlOp("<=", SqlComputed("current_date")).expr(pgDb, "date")).toEqual("date <= current_date")
-    expect(("date" lte SqlComputed("123")).second.expr(pgDb, "x")).toEqual("x <= 123")
+    @Test fun expressionValuesBindingStrategy() {
+      val and = andExpr("column" to Between(1, 2), "array" to listOf(3, 4))
+      expect(and.expr(db, "")).toEqual("(\"column\" between ? and ? and array in (?, ?))")
+      expect(and.values(db).toList()).toEqual(listOf<Any?>(1, 2, 3, 4))
+    }
   }
 }
